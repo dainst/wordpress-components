5de8e01aa4e171434dbcfbf842c5896f8161a723
paflov <philipp.franck@dainst.de>
2019-02-22 15:17:33 +0100

    add temporaray blocker to disable import function on prodicutive and test system

---
+++ plugins/shap-import-blocker/shap-importer-blocker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package shap-importer
+ * @version 0.0.1
+ */
+/*
+Plugin Name: shap-importer-b
+Plugin URI:  https://github.com/dainst/wordpress-sha--importer
+Description: --
+Author:	     Philipp Franck
+Author URI:	 http://www.dainst.org/
+Version:     0.0.1
+*/
+
+define("SHAP_BLOCKED", true);
